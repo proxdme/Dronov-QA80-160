843299bd0b360639dd20818a37cc0b166831493a
MikhailD <proxdme@mail.ru>
2024-03-28 11:58:15 +0300

    fast and Hash

--- src/main/java/ru/netology/Game.java
+++ src/main/java/ru/netology/Game.java
@@ -1,25 +1,33 @@
 package ru.netology;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Game {
-    private List<Player> registeredPlayers = new ArrayList<>();
+    //private List<Player> registeredPlayers = new ArrayList<>();
+    HashMap<Integer, Player> registeredPlayers = new HashMap<>();
 
     public void register(Player player) {
-        if (!registeredPlayers.contains(player)) {
-            registeredPlayers.add(player);
-        }
+        registeredPlayers.put(player.getId(), player);
     }
-
     public Player findByName(String name) {
-        for (Player player : registeredPlayers) {
-            if (player.name.equals(name)) {
+        for (Player player : registeredPlayers.values()) {
+            if (player.getName().equals(name)) {
                 return player;
             }
         }
         return null;
     }
+
+
+    //public void register(Player player) {
+    //   if (!registeredPlayers.contains(player)) {
+    //        registeredPlayers.add(player);
+     //   }
+     //}
+
+
 
     public int round(String playerName1, String playerName2) throws NotRegisteredException {
         try {
--- src/test/java/GameTests.java
+++ src/test/java/GameTests.java
@@ -11,13 +11,13 @@ public class GameTests {
     @Test
     public void shouldTestRegisteredPlayer() {
         Game game = new Game();
-        Player player1 = new Player(1, "Игрок1", 100);
-        Player player2 = new Player(2, "Игрок2", 90);
+        Player player1 = new Player(1, "Player1", 100);
+        Player player2 = new Player(2, "Player2", 90);
 
         game.register(player1);
         game.register(player2);
 
-        Assertions.assertEquals(player1, game.findByName("Игрок1"));
+        Assertions.assertEquals(player1, game.findByName("Player1"));
 
     }
 
@@ -27,65 +27,65 @@ public class GameTests {
 
 
         Assertions.assertThrows(NotRegisteredException.class, () -> {
-            game.round("Игрок1", "Игрок2");
+            game.round("Player1", "Player2");
         });
     }
 
     @Test
     public void shouldTestPlayerWhoWin() throws NotRegisteredException {
         Game game = new Game();
-        Player player1 = new Player(1, "Игрок1", 100);
-        Player player2 = new Player(2, "Игрок2", 90);
+        Player player1 = new Player(1, "Player1", 100);
+        Player player2 = new Player(2, "Player2", 90);
 
         game.register(player1);
         game.register(player2);
 
-        Assertions.assertEquals(1, game.round("Игрок1", "Игрок2"));
+        Assertions.assertEquals(1, game.round("Player1", "Player2"));
     }
 
     @Test
     public void shouldTestPlayerWhoLost() {
         Game game = new Game();
-        Player player1 = new Player(1, "Игрок1", 90);
-        Player player2 = new Player(2, "Игрок2", 100);
+        Player player1 = new Player(1, "Player1", 90);
+        Player player2 = new Player(2, "Player2", 100);
 
         game.register(player1);
         game.register(player2);
 
-        Assertions.assertEquals(2, game.round("Игрок1", "Игрок2"));
+        Assertions.assertEquals(2, game.round("Player1", "Player2"));
     }
 
     @Test
     public void shouldTestDrawGame() {
         Game game = new Game();
-        Player player1 = new Player(1, "Игрок1", 100);
-        Player player2 = new Player(2, "Игрок2", 100);
+        Player player1 = new Player(1, "Player1", 100);
+        Player player2 = new Player(2, "Player2", 100);
 
         game.register(player1);
         game.register(player2);
 
-        Assertions.assertEquals(0, game.round("Игрок1", "Игрок2"));
+        Assertions.assertEquals(0, game.round("Player1", "Player2"));
     }
 
     @Test
     public void shouldTestSecondPlayerUnregistered() {
         Game game = new Game();
-        Player player1 = new Player(1, "Игрок1", 100);
+        Player player1 = new Player(1, "Player1", 100);
 
         game.register(player1);
 
         Assertions.assertThrows(NotRegisteredException.class, () -> {
-            game.round("Игрок1", "Игорок2");
+            game.round("Player1", "Player2");
         });
     }
 
     @Test
     public void shouldTestRegisterNewPlayer() {
         Game game = new Game();
-        Player player1 = new Player(1, "Игорок1", 100);
+        Player player1 = new Player(1, "Player1", 100);
 
         game.register(player1);
-        Player expectedPlayer = game.findByName("Игорок1");
+        Player expectedPlayer = game.findByName("Player1");
         Assertions.assertEquals(player1, expectedPlayer);
     }
 
